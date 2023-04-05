31f88f469906528b8908d454f1aede538d69c8a3
Marcelo Santana Camacho <marcelocamacho.ufpa@gmail.com>
2023-04-05 11:05:46 -0300

    Primeira função do pacote que lista as chamadas da plataforma fazendo uso da API

---
+++ R/list_meetings.R
@@ -0,0 +1,23 @@
+list_meetings <- function(user_id){
+
+  user_id <- "marcelocamacho.ufpa@gmail.com"
+
+  meetings <- httr::GET(
+    paste0('https://api.zoom.us/v2/users/',user_id,'/meetings'),
+    httr::add_headers(authorization = paste("Bearer",Sys.getenv("ZOOM_API")))
+  )
+  ## Testando o objeto meetings
+   mensagem <- httr::content(meetings)$message
+
+  existing_meetings <- meetings %>%
+    httr::content() %>%
+    purrr::pluck("meetings") %>%
+    purrr::map(unlist,recursive = TRUE) %>%
+    purrr::map_dfr(tibble::enframe, .id = "order") %>%
+    tidyr::pivot_wider(
+      names_from = name,
+      values_from = value, names_repair = "unique"
+    )
+
+  existing_meetings
+}
