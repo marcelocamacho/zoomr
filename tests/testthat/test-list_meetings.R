3ce84f73fc4b68e7c5a80015d148f99867c5474e
Marcelo Santana Camacho <marcelocamacho.ufpa@gmail.com>
2023-04-05 12:33:58 -0300

    Correção do teste após a função de simplificação do existing_meetings que removeu a coluna 'order'

--- tests/testthat/test-list_meetings.R
+++ tests/testthat/test-list_meetings.R
@@ -1,5 +1,5 @@
 test_that("list_meetings works", {
   chamadas_marcelo <- list_meetings("marcelocamacho.ufpa@gmail.com")
   testthat::expect_s3_class(chamadas_marcelo,"tbl")
-  testthat::expect_equal(ncol(chamadas_marcelo),11)
+  testthat::expect_equal(ncol(chamadas_marcelo),10)
 })
